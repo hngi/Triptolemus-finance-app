1ca7ec15d5d40055b19c41e6d90c1db2a283a963
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-23 22:06:41 +0100

    items route

--- routes/Item/item.js
+++ routes/Item/item.js
@@ -1,16 +1,33 @@
 let express = require('express');
 let router = express.Router();
-let User=require('../../schema/user')
-let Item=require('../../schema/item')
+let User = require('../../schema/user');
+let Item = require('../../schema/item');
 
+router.post('/api/users/:userId/items', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name, description, amount } = req.body;
+    const newItem = new Item({
+      name,
+      description,
+      amount,
+      user_id: userId
+    });
+    const item = await newItem.save();
+    res.json({ item: item });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+router.get('/api/users/:userId/items', async (req, res) => {
+  try {
+    const { userId } = req.params;
 
-router.get('/api/users/:userId/items',async(req,res)=>{
-try {
-  const user=await User.find({})
-  console.log(user);
-} catch (error) {
-  
-}
-})
+    const items = await Item.find({ user_id: userId });
+    res.json({ items: items });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
-module.exports=router;
+module.exports = router;
