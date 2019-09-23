1b8fd77552b868e119ae1685133eb901d922b5db
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-23 23:51:31 +0100

    cleanup route

--- routes/Auth/register.js
+++ routes/Auth/register.js
@@ -1,51 +1,40 @@
 require('../../database/database');
 const jwt = require('jsonwebtoken');
-let UserModel = require('../../schema/user')
+let UserModel = require('../../schema/user');
 const express = require('express');
 const router = express.Router();
 
-
 router.post('/api/auth/register', async (req, res) => {
-    let username = req.body.username;
-    let email = req.body.email;
-    let password = req.body.password;
-    if ((username == "" || username == undefined) || (email == "" || email == undefined) || (password == "" || password == undefined)) {
-        res.status(401)
-        res.json({ error: "Input Fields can't be empty" })
-    } else {
-
-        try {
-
-            let user = new UserModel({
-                username: username,
-                email: email,
-                password: password
-            })
-            let userDoc = await user.save()
-            userDocJson = userDoc.toJSON()
-            delete userDocJson.password
-            userDocJson["token"] = jwt.sign({ _id: user._id }, process.env.JWT_KEY)
-            res.status(200)
-            res.json(userDocJson)
-        } catch (error) {
-            res.status(401)
-            if (error.errmsg.toString().includes("duplicate key error collection")) {
-                res.json({ error: "User already exist" })
-            } else {res.json({ error: error.errmsg.toString })
-
-            }
-
-
-
-
-
-        }
-
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  if (
+    username == '' ||
+    username == undefined ||
+    (email == '' || email == undefined) ||
+    (password == '' || password == undefined)
+  ) {
+    return res.status(401).json({ error: "Input Fields can't be empty" });
+  } else {
+    try {
+      let user = new UserModel({
+        username: username,
+        email: email,
+        password: password
+      });
+      let userDoc = await user.save();
+      userDocJson = userDoc.toJSON();
+      delete userDocJson.password;
+      userDocJson['token'] = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
+      res.status(200).json(userDocJson);
+    } catch (error) {
+      if (error.errmsg.toString().includes('duplicate key error collection')) {
+        return res.status(401).json({ error: 'User already exists' });
+      } else {
+        res.status(400).json({ error: error.errmsg.toString });
+      }
     }
+  }
+});
 
-
-})
-
-
-
-module.exports = router
+module.exports = router;
