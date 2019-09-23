a3d25c646ecae64cd07acfb586e4220230c89eea
Winning <winninggreat@gmail.com>
2019-09-23 15:07:33 -0800

    Updated registration route

--- routes/Auth/register.js
+++ routes/Auth/register.js
@@ -1,28 +1,50 @@
 require('../../database/database');
+const jwt = require('jsonwebtoken');
 let UserModel = require('../../schema/user')
 const express = require('express');
 const router = express.Router();
 
 
-router.post('/api/v1/auth/register', function(req,res){
+router.post('/api/auth/register', async (req,res)=>{
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
+    if ((username==""||username==undefined)||(email=="" ||email==undefined)||(password==""||password==undefined)){
+        res.status(401)
+        res.json({error:"Input Fields can't be empty"})
+    } else {
+
+    try{
+        
     let user = new UserModel({
         username: username,
         email: email,
         password: password
     })
-    user.save()
-        .then(doc => {
-            let jsonDoc = doc.toJSON();
-            delete jsonDoc.password;
-            res.json(jsonDoc);
-        })
-        .catch(err=>{
-            console.log(err);
-            res.json({"registered":false});
-        })
+    let userDoc = await user.save()
+    userDocJson = userDoc.toJSON()
+    delete userDocJson.password
+    userDocJson["token"] = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+    res.status(200)
+    res.json(userDocJson)
+        } catch (error){
+            res.status(401)
+            console.log(error)
+            if (error.errmsg.toString().includes("duplicate key error collection")){
+                res.json({error:"User already exist"})
+            } else{
+                console.log(error)
+                res.json({error:error.errmsg.toString})
+
+            }
+
+
+
+
+
+        }
+        
+    }
 
 
 })
