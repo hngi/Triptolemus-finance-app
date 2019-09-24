24b84c57495ae6290628d569b4ae3f9f10cae602
Winning <winninggreat@gmail.com>
2019-09-24 01:10:29 -0800

    Modified Auth middleware

--- routes/Auth/register.js
+++ routes/Auth/register.js
@@ -26,12 +26,12 @@ router.post('/api/auth/register', async (req, res) => {
       userDocJson = userDoc.toJSON();
       delete userDocJson.password;
       userDocJson['token'] = jwt.sign({ _id: user._id }, process.env.JWT_KEY);
-      res.status(200).json(userDocJson);
+      return res.status(200).json(userDocJson);
     } catch (error) {
       if (error.errmsg.toString().includes('duplicate key error collection')) {
         return res.status(401).json({ error: 'User already exists' });
       } else {
-        res.status(400).json({ error: error.errmsg.toString });
+        return res.status(400).json({ error: error.errmsg.toString });
       }
     }
   }
