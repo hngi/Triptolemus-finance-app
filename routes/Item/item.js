f58ba616d20bf7bf766445437c2d8c0153938290
Mr-emeka <speak2c.emeka@gmail.com>
2019-09-24 07:53:14 +0100

    calc year route

--- routes/Item/item.js
+++ routes/Item/item.js
@@ -1,16 +1,39 @@
 let express = require('express');
 let router = express.Router();
-let User=require('../../schema/user')
-let Item=require('../../schema/item')
+let User = require('../../schema/user')
+let Item = require('../../schema/item')
 
 
-router.get('/api/users/:userId/items',async(req,res)=>{
-try {
-  const user=await User.find({})
-  console.log(user);
-} catch (error) {
-  
-}
+router.get('/api/users/:userId/items', async (req, res) => {
+  try {
+    const user = await User.find({})
+    console.log(user);
+  } catch (error) {
+
+  }
 })
 
-module.exports=router;
+
+
+/**
+ * calculate total spent in a year
+ * @params {object} req
+ * @params {object} res
+ * @returns {object} With total spent in a year
+ */
+router.get('/api.users/:userId/calculate/year', async (req, res) => {
+  const {
+    userId
+  } = req.params;
+  try {
+    const user = await User.findById({
+      id: userID
+    })
+  } catch (e) {
+    res.status(400).json({
+      message: 'An error occurred'
+    })
+  }
+})
+
+module.exports = router;
