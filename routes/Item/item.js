7184eb2805ea464f9172ae190a207a6dc06df448
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-24 17:45:48 +0100

    modify middleware

--- routes/Item/item.js
+++ routes/Item/item.js
@@ -1,31 +1,45 @@
 let express = require('express');
 let router = express.Router();
-let userAuth = require('../../middleware/userAuth')
-let User = require('../../schema/user');
+let userAuth = require('../../middleware/userAuth');
 let Item = require('../../schema/item');
 
-router.post('/api/users/:userId/items',userAuth.verifyToken, async (req, res) => {
+router.post('/api/users/:userId/items', userAuth, async (req, res) => {
   try {
     const { userId } = req.params;
+    const id = req.user;
+    if (userId !== id) {
+      return res.status(401).json({ error: 'Unauthorized user' });
+    }
+
     const { name, description, amount } = req.body;
+    if (name === '' || description === '' || amount === '') {
+      res.status(400).json({ error: 'Input field cannot be empty' });
+    }
     const newItem = new Item({
       name,
       description,
       amount,
-      user_id: userId
+      user_id: id,
+      date: new Date()
     });
     const item = await newItem.save();
-    res.json({ item: item });
+    res.status(200).json({ item: item });
   } catch (error) {
     console.log(error.message);
   }
 });
-router.get('/api/users/:userId/items',userAuth.verifyToken ,async (req, res) => {
+router.get('/api/users/:userId/items', userAuth, async (req, res) => {
   try {
     const { userId } = req.params;
-
-    const items = await Item.find({ user_id: userId });
-    res.json({ items: items });
+    const id = req.user;
+    if (userId !== id) {
+      return res.status(401).json({ error: 'Unauthorized user' });
+    }
+    const items = await Item.find({ user_id: id });
+    if (!items) {
+      res.status(200).json({ items: null });
+    }
+    res.status(200).json({ items: items });
   } catch (error) {
     console.log(error.message);
   }
