989d28914c38f46c2307ea2277bee24a066dfd0c
Ismaila Hassan <ismailah28@gmail.com>
2019-09-24 14:24:17 +0100

    Implement getting ittem by month and date range

--- routes/Item/item.js
+++ routes/Item/item.js
@@ -31,4 +31,39 @@ router.get('/api/users/:userId/items',userAuth.verifyToken ,async (req, res) => 
   }
 });
 
+//route for getting all items in date range
+router.get('/api/users/:userId/calculate/:startDate/:endDate', (req,res, next) => {
+  const { userId, startdate, enddate } = req.params;
+  const newItem = Item.find({user_id: userId, date: {$gte: startDate, $lte: endDate}}).then(
+    things => {
+      console.log(things);
+      res.status(200).json(things);
+    }
+  ).catch(
+    error => {
+      console.log(error)
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+// route for getting all items for a particular month
+router.get('/api/users/:userId/calculate/:month', userAuth.verifyToken, (req, res, next) => {
+  const { userId, month } = request.params;
+  const newItems = Item.find({$where: function(){ return this.date.getMonth() == month} }).then(
+    things => {
+      console.log(things);
+      res.status(200).json(things);
+    }
+  ).catch(
+    error => {
+      res.status(404).json({
+        error: error
+      })
+    }
+  )
+});
+
 module.exports = router;
