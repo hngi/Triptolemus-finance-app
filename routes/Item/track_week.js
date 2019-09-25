f6f4bcaa25f3098f6d6a38d0de481513a7200ab6
teevona <tegovona@gmail.com>
2019-09-25 07:07:53 +0100

    week-track

--- routes/Item/track_week.js
+++ routes/Item/track_week.js
@@ -1,10 +1,10 @@
-require('../database/database');
 const express = require('express');
 const router = express.Router();
+//let moment = require('moment')
 let mongoose = require('mongoose')
 let itemModel = require('../schema/item');
 let userModel = require('../schema/user');
-//let validator = require('validator')
+let validator = require('validator')
 var app = express();
 
 Date.prototype.addDays = function(days) {
@@ -12,6 +12,32 @@ Date.prototype.addDays = function(days) {
     date.setDate(date.getDate() + days);
     return date;
 }
+
+router.post('/api/users/:userId/calculate/week', async (req, res) => {
+	let userId = req.userId, startDate = req.body.startDate, endDate = req.body.endDate;
+	var end_msec = Date.parse(endDate);
+	end_date = new Date(end_msec);
+	endDate = end_date.addDays(1);
+	itemModel.find({
+		user_id : userId,
+		date: { $gte: startDate, $lte: endDate }
+	}).sort({date: 1})
+	.then(doc => {
+		if(doc.length > 0) {
+			let docCount = doc.length;
+			weekCost = 0;
+			for(i=0; i<docCount; i++) {
+	 			weekCost = weekCost + doc[i].amount;
+	 		}
+	 		return res.status(200).json({weeklyCost: weekCost, startdate: startDate, enddate: endDate});
+		} else {
+			return res.status(400).json();
+		}
+	})
+	.catch(err => {
+	 	console.error(err)
+	})
+});
 
 app.get('/users/:userId/calculate/week', function (req, res) {
 	//user_id = req.params.userId;
