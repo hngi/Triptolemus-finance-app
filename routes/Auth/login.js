dbb3ca7c388c20b65d13e4e57b388b34dee6f8bc
Winning <winninggreat@gmail.com>
2019-09-25 20:48:02 -0800

    removed password from login response

--- routes/Auth/login.js
+++ routes/Auth/login.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 
 router.post('/api/auth/login', async (req, res) => {
+  console.log(req.body)
   const email = req.body.email,
     password = req.body.password;
 
@@ -23,9 +24,11 @@ router.post('/api/auth/login', async (req, res) => {
       bcrypt.compare(password, user.password).then(isMatch => {
         if (isMatch) {
           token = jwt.sign({ _id: user._id }, 'secret');
+          userJSON = user.toJSON()
+          delete userJSON.password
           return res.status(200).json({
             token,
-            user
+            userJSON
           });
         } else {
           return res.status(401).json({
