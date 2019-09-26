bc5f9277fce4254fcb0c378b63504e15b486d1ac
teevona <tegovona@gmail.com>
2019-09-26 08:30:06 +0100

    altered weekly

--- routes/Item/item.js
+++ routes/Item/item.js
@@ -122,34 +122,45 @@ router.post('/api/users/:userId/calculate/week', userAuth, async (req, res) => {
     })
       .sort({ date: 1 })
       .then(doc => {
-        if (doc.length > 0) {
+        //if (doc.length > 0) {
           let docCount = doc.length;
           weeklies = [];
-          week_index = 0;
+          //week_index = 0;
+          totalCost = 0;
           weekCost = 0;
           prev_week = 0;
           for (i = 0; i < docCount; i++) {
             if (i>0) { prev_week = item_week; }
             item_week = moment(doc[i].date).week()
             if (item_week == prev_week) {
+              totalCost = totalCost + doc[i].amount;
               weekCost = weekCost + doc[i].amount;
               if ((i+1) == docCount) { 
-                weeklies[week_index] = { weeklyCost: weekCost, weekNumber: item_week } 
+                //weeklies[week_index] = { weeklyCost: weekCost, weekNumber: item_week } 
+                weeklies.push({ weeklyCost: weekCost, weekNumber: "Week "+item_week }); 
               }
             } else {
-                weeklies[week_index] = { weeklyCost: weekCost, weekNumber: prev_week }
+                //weeklies[week_index] = { weeklyCost: weekCost, weekNumber: prev_week }
+                weeklies.push({ weeklyCost: weekCost, weekNumber: "Week "+prev_week });
                 if (i > 0) { 
-                  week_index++; 
+                  //week_index++; 
                   weekCost = 0;
+                }
+                totalCost = totalCost + doc[i].amount;
+                weekCost = weekCost + doc[i].amount;
+                if ((i+1) == docCount) { 
+                  //weeklies[week_index] = { weeklyCost: weekCost, weekNumber: item_week } 
+                  weeklies.push({ weeklyCost: weekCost, weekNumber: "Week "+item_week });  
                 }
             }
           }
-          res.status(200).json(weeklies);
-        } else {
-          res
-            .status(400)
-            .json({ weeklyCost: 0, startdate: startDate, enddate: endDate });
-        }
+          weekly_track = { totalExpenses: totalCost, expensePerWeek : weeklies}
+          res.status(200).json(weekly_track);
+        // } else {
+        //   res
+        //     .status(400)
+        //     .json({ weeklyCost: 0, startdate: startDate, enddate: endDate });
+        // }
       })
       .catch(err => {
         console.error(err);
