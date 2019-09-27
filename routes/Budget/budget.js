834fb69fff8244bec360ab102e15026e61d0b452
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-27 23:00:13 +0100

    budget path

--- routes/Budget/budget.js
+++ routes/Budget/budget.js
@@ -1,46 +1,40 @@
 let express = require('express');
 let router = express.Router();
-<<<<<<< HEAD
 let User = require('../../schema/user');
 const mongoose = require('mongoose');
-let userAuth = require('../../middleware/userAuth');
-=======
-let userAuth = require('../../middleware/userAuth');
-let User=require('../../schema/user')
->>>>>>> ba8e5f4d9fa31a8795b2e7f85ee31505095a08a3
+const userAuth = require('../../middleware/userAuth');
 
-router.put('/api/users/:userId/update_budget',userAuth,async(req,res)=>{
+router.put('/api/users/:userId/update_budget', userAuth, async (req, res) => {
   try {
     const { userId } = req.params;
-    const { duration,budget } = req.body;
+    const { duration, budget } = req.body;
     const id = req.user;
     if (userId !== id) {
       return res.status(401).json({
         error: 'Unauthorized user'
       });
-    }
-
-    else if(duration === 'weekly'){
-      const newBudget= await User.findByIdAndUpdate(id,{weekly_budget:budget})
+    } else if (duration == 'Weekly') {
+      const newBudget = await User.findByIdAndUpdate(id, {
+        weekly_budget: budget
+      });
       return res.status(200).json({
         budget: newBudget
       });
-    }
-    else if(duration === 'monthly'){
-      const newBudget = await User.findByIdAndUpdate(id,{
-        
+    } else if (duration == 'Monthly') {
+      const newBudget = await User.findByIdAndUpdate(id, {
         monthly_budget: budget
       });
       return res.status(200).json({
         budget: newBudget
       });
-    }
-    else if(duration === 'yearly'){
-      const newBudget = await User.findByIdAndUpdate(id,{  yearly_budget: budget });
+    } else if (duration == 'Yearly') {
+      const newBudget = await User.findByIdAndUpdate(id, {
+        yearly_budget: budget
+      });
       return res.status(200).json({
         budget: newBudget
       });
-    }else{
+    } else {
       return res.status(400).json({
         error: 'Please enter either weekly,monthly or yearly'
       });
@@ -50,7 +44,49 @@ router.put('/api/users/:userId/update_budget',userAuth,async(req,res)=>{
       error: 'Error updating budget'
     });
   }
-})
+});
+router.post('/api/users/:userId/setWeeklyBudget', userAuth, async (req, res) => {
+  try {
+		const { userId } = req.params;
+        const id = req.user;
+        let { budget } = req.body;
+        if (userId !== id) {
+            return res.status(401).json({ error: 'Unauthorized user' });
+        }
+        let user = await User.find({
+          id: userId
+        });
+        user.weekly_budget = budget;
+        const updated_user = await user.save();
+        res.status(200).json({
+          user: updated_user
+        });
+  } catch (error) {
+        console.log(error.message);
+  }
+});
+​
+router.post('/api/users/:userId/setYearlyBudget', userAuth, async (req, res) => {
+  try {
+		const { userId } = req.params;
+        const id = req.user;
+        let { budget } = req.body;
+        if (userId !== id) {
+            return res.status(401).json({ error: 'Unauthorized user' });
+        }
+        let user = await User.find({
+          id: userId
+      });
+        user.yearly_budget = budget;
+        const updated_user = await user.save();
+        res.status(200).json({
+          user: updated_user
+        });
+  } catch (error) {
+        console.log(error.message);
+  }
+});
+​
 router.put('/api/users/:userId/setMonthlyBudget', userAuth, async (req, res, next) => {
     try {
         const { userId } = req.params;
@@ -76,7 +112,7 @@ router.put('/api/users/:userId/setMonthlyBudget', userAuth, async (req, res, nex
                 })
             }
         );
-
+​
     } catch (error) {
         console.log(error.message);
     }
