ba8e5f4d9fa31a8795b2e7f85ee31505095a08a3
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-27 21:52:25 +0100

    ,,

--- routes/Budget/budget.js
+++ routes/Budget/budget.js
@@ -1,57 +1,8 @@
 let express = require('express');
 let router = express.Router();
-<<<<<<< HEAD
 let userAuth = require('../../middleware/userAuth');
 let User=require('../../schema/user')
-=======
-let User = require('../../schema/user');
-const mongoose = require('mongoose');
-const userAuth = require('../../middleware/userAuth');
 
-router.post('/api/users/:userId/setWeeklyBudget', userAuth, async (req, res) => {
-  try {
-		const { userId } = req.params;
-        const id = req.user;
-        let { budget } = req.body;
-        if (userId !== id) {
-            return res.status(401).json({ error: 'Unauthorized user' });
-        }
-        let user = await User.find({
-          id: userId
-        });
-        user.weekly_budget = budget;
-        const updated_user = await user.save();
-        res.status(200).json({
-          user: updated_user
-        });
-  } catch (error) {
-        console.log(error.message);
-  }
-});
-
-router.post('/api/users/:userId/setYearlyBudget', userAuth, async (req, res) => {
-  try {
-		const { userId } = req.params;
-        const id = req.user;
-        let { budget } = req.body;
-        if (userId !== id) {
-            return res.status(401).json({ error: 'Unauthorized user' });
-        }
-        let user = await User.find({
-          id: userId
-      });
-        user.yearly_budget = budget;
-        const updated_user = await user.save();
-        res.status(200).json({
-          user: updated_user
-        });
-  } catch (error) {
-        console.log(error.message);
-  }
-});
->>>>>>> ed5568a39e5734433c252265595de13ecb88adef
-
-<<<<<<< HEAD
 router.put('/api/users/:userId/update_budget',userAuth,async(req,res)=>{
   try {
     const { userId } = req.params;
@@ -94,7 +45,6 @@ router.put('/api/users/:userId/update_budget',userAuth,async(req,res)=>{
     });
   }
 })
-=======
 router.put('/api/users/:userId/setMonthlyBudget', userAuth, async (req, res, next) => {
     try {
         const { userId } = req.params;
@@ -126,5 +76,4 @@ router.put('/api/users/:userId/setMonthlyBudget', userAuth, async (req, res, nex
     }
 })
 
->>>>>>> c3b11b483f193a45e1e5f7f7fbf5034714016ba1
 module.exports = router;
