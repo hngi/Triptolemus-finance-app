cccf378c81282c523968c450bc6880e8b1fd9042
Ismaila Hassan <ismailah28@gmail.com>
2019-09-27 18:54:24 +0100

    Implement setting monthly budget

--- routes/Budget/budget.js
+++ routes/Budget/budget.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let router = express.Router();
 let User = require('../../schema/user');
 const mongoose = require('mongoose');
+userAuth = require('../../middleware/userAuth');
 
 router.post('/api/users/:userId/setWeeklyBudget', userAuth, async (req, res) => {
   try {
@@ -45,5 +46,37 @@ router.post('/api/users/:userId/setYearlyBudget', userAuth, async (req, res) => 
         console.log(error.message);
   }
 });
+
+
+router.put('/api/users/:userId/setMonthlyBudget', userAuth, async (req, res, next) => {
+    try {
+        const { userId } = req.params;
+        const { duration, amount } = req.body;
+        const id = req.user;
+        
+        if (userId !== id) {
+            return res.status(401).json({
+                error: 'Unauthorized user'
+            });
+        }
+        const newBudget = new User({ _id: userId })
+        await User.updateOne({_id: userId},{ $set: {monthly_budget: amount}}).then(
+            () => {
+                res.status(201).json({
+                    message: "Budget set successfully"
+                });
+            }
+        ).catch(
+            error => {
+                res.status(400).json({
+                    error: error
+                })
+            }
+        );
+
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 module.exports = router;
