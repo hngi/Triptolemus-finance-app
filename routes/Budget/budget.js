e85f625d3f9f456f7ce9b4f3806cd531d065b4da
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-28 19:34:50 +0100

    budget

--- routes/Budget/budget.js
+++ routes/Budget/budget.js
@@ -182,4 +182,46 @@ router.post('/api/users/:userId/setMonthlyBudget', async (req, res) => {
 //   }
 // });
 
+router.put('/api/users/:userId/update_budget',userAuth,async(req,res)=>{
+  try {
+    const { userId } = req.params;
+    const { duration,budget } = req.body;
+    const id = req.user;
+    if (userId !== id) {
+      return res.status(401).json({
+        error: 'Unauthorized user'
+      });
+    }
+
+    else if(duration === 'weekly'){
+      const newBudget= await User.findByIdAndUpdate(id,{weekly_budget:budget})
+      return res.status(200).json({
+        budget: newBudget
+      });
+    }
+    else if(duration === 'monthly'){
+      const newBudget = await User.findByIdAndUpdate(id,{
+        
+        monthly_budget: budget
+      });
+      return res.status(200).json({
+        budget: newBudget
+      });
+    }
+    else if(duration === 'yearly'){
+      const newBudget = await User.findByIdAndUpdate(id,{  yearly_budget: budget });
+      return res.status(200).json({
+        budget: newBudget
+      });
+    }else{
+      return res.status(400).json({
+        error: 'Please enter either weekly,monthly or yearly'
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: 'Error updating budget'
+    });
+  }
+})
 module.exports = router;
--- view/src/actions/expense.js
+++ view/src/actions/expense.js
@@ -5,30 +5,34 @@ import {
   FETCH_MONTHLY_EXPENSE_FAIL,
   FETCH_YEARLY_EXPENSE_SUCCESS,
   FETCH_YEARLY_EXPENSE_FAIL
-  
 } from './types';
 // import { setAlert } from './alert';
 import axios from 'axios';
-// const base_url = 'https://finance-tracker-server.herokuapp.com';
-const base_url = 'http://localhost:3500';
+const base_url = 'https://finance-tracker-server.herokuapp.com';
+// const base_url = 'http://localhost:3500';
 
-export const getWeeklyExpense = (userIdd) => async dispatch => {
-let curr = new Date();
-let week = [];
+export const getWeeklyExpense = userIdd => async dispatch => {
+  let curr = new Date();
+  let week = [];
 
-for (let i = 1; i <= 7; i++) {
-  let first = curr.getDate() - curr.getDay() + i;
-  let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
-  week.push(day);
-}  
- const body = JSON.stringify({
-   startDate:week[0],endDate:week[6]
- });
- const config = {
-   headers: { 'Content-Type': 'application/json' }
- };
-try {
-    const response = await axios.post(base_url + `/api/users/${userIdd}/calculate/week`,body,config);
+  for (let i = 1; i <= 7; i++) {
+    let first = curr.getDate() - curr.getDay() + i;
+    let day = new Date(curr.setDate(first)).toISOString().slice(0, 10);
+    week.push(day);
+  }
+  const body = JSON.stringify({
+    startDate: week[0],
+    endDate: week[6]
+  });
+  const config = {
+    headers: { 'Content-Type': 'application/json' }
+  };
+  try {
+    const response = await axios.post(
+      base_url + `/api/users/${userIdd}/calculate/week`,
+      body,
+      config
+    );
     console.log(response);
     dispatch({
       type: FETCH_WEEKLY_EXPENSE_SUCCESS,
@@ -44,12 +48,12 @@ try {
     // dispatch(setAlert(error.response.data.error, 'danger'));
   }
 };
-export const getMonthlyExpense = (userIdd) => async dispatch => {
- var date = new Date();
+export const getMonthlyExpense = userIdd => async dispatch => {
+  var date = new Date();
 
- var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
 
- var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0); 
+  var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   // console.log(month);
   const body = JSON.stringify({
     startDate: firstDay,
@@ -59,7 +63,11 @@ export const getMonthlyExpense = (userIdd) => async dispatch => {
     headers: { 'Content-Type': 'application/json' }
   };
   try {
-    const response = await axios.post(base_url + `/api/users/${userIdd}/calculate/month`,body,config);
+    const response = await axios.post(
+      base_url + `/api/users/${userIdd}/calculate/month`,
+      body,
+      config
+    );
     console.log(response);
     dispatch({
       type: FETCH_MONTHLY_EXPENSE_SUCCESS,
@@ -75,18 +83,22 @@ export const getMonthlyExpense = (userIdd) => async dispatch => {
     // dispatch(setAlert(error.response.data.error, 'danger'));
   }
 };
-export const getYearlyExpense = (userIdd) => async dispatch => {
+export const getYearlyExpense = userIdd => async dispatch => {
   let firstDay = new Date(new Date().getFullYear(), 0, 1).toISOString();
-let lastDay = new Date().toISOString();
-const body = JSON.stringify({
-  startDate: firstDay,
-  endDate: lastDay
-});
-const config = {
-  headers: { 'Content-Type': 'application/json' }
-};
+  let lastDay = new Date().toISOString();
+  const body = JSON.stringify({
+    startDate: firstDay,
+    endDate: lastDay
+  });
+  const config = {
+    headers: { 'Content-Type': 'application/json' }
+  };
   try {
-    const response = await axios.post(base_url + `/api/users/${userIdd}/calculate/year`,body,config);
+    const response = await axios.post(
+      base_url + `/api/users/${userIdd}/calculate/year`,
+      body,
+      config
+    );
     console.log(response);
     dispatch({
       type: FETCH_YEARLY_EXPENSE_SUCCESS,
