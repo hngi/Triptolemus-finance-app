50af3c7c4d93efe2b56bc01a0d4b0573a74934a9
Busola <olowubusolaolutayo1992@gmail.com>
2019-09-29 12:17:40 +0100

    .

--- view/src/actions/expense.js
+++ view/src/actions/expense.js
@@ -8,8 +8,8 @@ import {
 } from './types';
 // import { setAlert } from './alert';
 import axios from 'axios';
-// const base_url = 'https://finance-tracker-server.herokuapp.com';
-const base_url = 'http://localhost:3500';
+const base_url = 'https://finance-tracker-server.herokuapp.com';
+// const base_url = 'http://localhost:3500';
 
 export const getWeeklyExpense = userIdd => async dispatch => {
   let curr = new Date();
