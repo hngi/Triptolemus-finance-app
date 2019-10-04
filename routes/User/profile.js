41b068676b66a2c554c39e7d4a4f7c8275192a62
Collins Onyetube <kosisochukwu.onyetube@fasylgh.com>
2019-10-04 16:50:28 +0000

    changed variable name

--- routes/User/profile.js
+++ routes/User/profile.js
@@ -32,18 +32,18 @@ router.get('/api/users/:userId/profile', async (req, res) => {
   }
 });
 
-router.put('/api/users/:user_id/profile' , async (req, res) => {
+router.put('/api/users/:userId/profile' , async (req, res) => {
   try {
-    const user_id = req.params
+    const userId = req.params
     const { first_name, last_name, email, phone_number, gender, date_of_birth } = req.body;
-    await User.find({ _id: user_id }).then( user =>{
+    await User.find({ _id: userId }).then( user =>{
         if(!user){
             return res.status(200).json({
               message: "No User found",
               success: false
             });
         }else {
-            User.findOneAndUpdate({_id: user_id}, {$set:{first_name: first_name, last_name: last_name,
+            User.findOneAndUpdate({_id: userId}, {$set:{first_name: first_name, last_name: last_name,
                  email: email, phone_number: phone_number, gender: gender, date_of_birth: date_of_birth}},{new: true})
                  .then( resp =>{
                      if(resp){
